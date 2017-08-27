547bbea317a50a57ce4078ff20b729b280ccafc7
Colin Mackenzie <nilocski@yahoo.com>
2017-08-28 00:16:05 +0200

    new branch test thing

---
+++ JustTesting/justAtest.swift
@@ -0,0 +1,13 @@
+//
+//  justAtest.swift
+//  JustTesting
+//
+//  Created by Colin Mackenzie on 28/08/2017.
+//  Copyright © 2017 Colin Mackenzie. All rights reserved.
+//
+
+import UIKit
+
+class justAtest: NSObject {
+
+}
