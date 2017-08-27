9ce78709f9ab5d49e3a778fe28898e2abf588996
Colin Mackenzie <nilocski@yahoo.com>
2017-08-27 23:35:53 +0200

    Initial Commit

---
+++ JustTestingTests/JustTestingTests.swift
@@ -0,0 +1,36 @@
+//
+//  JustTestingTests.swift
+//  JustTestingTests
+//
+//  Created by Colin Mackenzie on 27/08/2017.
+//  Copyright © 2017 Colin Mackenzie. All rights reserved.
+//
+
+import XCTest
+@testable import JustTesting
+
+class JustTestingTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
